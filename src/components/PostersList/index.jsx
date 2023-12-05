2e4018f54a7ab314de9b359ea4e284306c8f7801
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2023-12-05 20:11:11 -0300

    chore: Refatorando os botões das setinhas

--- src/components/PostersList/index.jsx
+++ src/components/PostersList/index.jsx
@@ -1,3 +1,4 @@
+import ArrowButton from "../ArrowButton"
 import Poster from "../Poster"
 import styles from "./PostersList.module.css"
 import { useEffect, useState, useRef } from "react"
@@ -49,15 +50,13 @@ function PostersList({ info }) {
         <section className={styles.main_container}>
           <h2>{info.sectionTitle}</h2>
           <div className={styles.contentListContainer}>
-            {containerScrollLeft !== 0 &&
-              <button onClick={scrollPrev} className={`${styles.contentListIconContainer} ${styles.iconArrowLeft}`}>
-                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg>
-              </button>
+              {/*Só exibe a setinha da esquerda se já foi feito algum scroll para a direita*/}
+              {containerScrollLeft !== 0 &&
+                <ArrowButton scroll={() => scrollPrev()} direction="Left"/>
               }
+              {/*Só exibe a setinha da direita se não chegou ao máximo scroll possível*/}
               { ref_contentList.current && containerScrollLeft !== (ref_contentList.current.scrollWidth - ref_contentList.current.clientWidth) &&
-              <button onClick={scrollNext} className={`${styles.contentListIconContainer} ${styles.iconArrowRight}`}>
-                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg>
-              </button>
+                <ArrowButton scroll={() => scrollNext()} direction="Right" />
               }
               <ul ref={ref_contentList} className={styles.contentList}>
                 {postersContent.results && postersContent.results.map((content) => {       
