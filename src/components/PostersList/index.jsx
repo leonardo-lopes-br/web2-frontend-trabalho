89acc1f9952efd57830488b8275aa0f606b2dea3
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2023-12-05 19:45:26 -0300

    chore: melhorando as setinhas

--- src/components/PostersList/index.jsx
+++ src/components/PostersList/index.jsx
@@ -5,10 +5,8 @@ import { useEffect, useState, useRef } from "react"
 function PostersList({ info }) {
 
     const [postersContent, setpostersContent] = useState({})
-    const ref_contentList = useRef(null)
-    const ref_iconArrowLeft = useRef(null)
-    const ref_iconArrowRight = useRef(null)
     const [containerScrollLeft, setContainerScrollLeft] = useState(0)
+    const ref_contentList = useRef(null)
 
     async function searchContent() {
             const urlpostersContent = info.baseUrl
@@ -19,9 +17,9 @@ function PostersList({ info }) {
     }
 
 
-  useEffect(() => {
-      searchContent()
-  }, [])
+    useEffect(() => {
+        searchContent()
+    }, [])
 
     
     const scrollNext = () => {
@@ -52,12 +50,12 @@ function PostersList({ info }) {
           <h2>{info.sectionTitle}</h2>
           <div className={styles.contentListContainer}>
             {containerScrollLeft !== 0 &&
-              <button ref={ref_iconArrowLeft} onClick={scrollPrev} className={`${styles.contentListIconContainer} ${styles.iconArrowLeft}`}>
+              <button onClick={scrollPrev} className={`${styles.contentListIconContainer} ${styles.iconArrowLeft}`}>
                 <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg>
               </button>
               }
               { ref_contentList.current && containerScrollLeft !== (ref_contentList.current.scrollWidth - ref_contentList.current.clientWidth) &&
-              <button ref={ref_iconArrowRight} onClick={scrollNext} className={`${styles.contentListIconContainer} ${styles.iconArrowRight}`}>
+              <button onClick={scrollNext} className={`${styles.contentListIconContainer} ${styles.iconArrowRight}`}>
                 <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg>
               </button>
               }
