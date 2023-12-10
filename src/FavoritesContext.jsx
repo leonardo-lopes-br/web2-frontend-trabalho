4640b76493a5a5ccadfaeea373f53d0fddf722b1
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2023-12-10 08:36:48 -0300

    chore: arrumando os favoritos

--- src/FavoritesContext.jsx
+++ src/FavoritesContext.jsx
@@ -6,16 +6,16 @@ export const FavoritesProvider = ({ children }) => {
   const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites')));
 
   function toggleFavorites(content) {
-    
+
     setFavorites(prevFavorites => {
         // primeiro a ser salvo
-        if (!localStorage.getItem('favorites')) {
+        if (!favorites || favorites.length === 0) {
             localStorage.setItem('favorites', JSON.stringify([content]))
             return [content]
         }
         else {
             // Já tem, vamos remover
-            if (prevFavorites.includes(content)) {
+            if (prevFavorites.some(item => item.id === content.id)) {
                 const newArray = prevFavorites.filter(favorite => favorite.id !== content.id)
                 localStorage.setItem('favorites', JSON.stringify(newArray))
                 return newArray
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,16 @@
 import styles from "./Home.module.css"
 import PostersList from "../../components/PostersList"
 import { postersInfo } from "../../data"
+import { useFavorites } from "../../FavoritesContext"
+import { useEffect } from "react"
 
 function Home() {
+    const { favorites } = useFavorites()
+
+    // apenas para re-renderizar a Home e conseguir atualizar Posters iguais em diferentes PostersList
+    // toda vez que os favoritos mudarem
+    useEffect(() => {}, [favorites])
+    
     return (
         <main className={styles.main_container}>
             {postersInfo.map((posterInfo) => <PostersList
