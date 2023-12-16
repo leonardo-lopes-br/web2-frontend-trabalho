408af668a7f7757d7f4200c57533c2e0a9ba67f5
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2023-12-16 11:02:01 -0300

    chore: incluindo primeira logica de filtro

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,9 +7,27 @@ function Header() {
 
     const [showSearchMobileOverlay, setShowSearchMobileOverlay] = useState(false);
 
+    function evalQuery (filter='') {
+        console.log('filtro: >' + filter + '<')
+    }
 
-    function searchUserQuery() {
-        setShowSearchMobileOverlay(false)
+    function searchQuery() {
+        // se a tela for pequena
+        if (window.innerWidth <= 620) {
+            // se já estava exibindo o input, usuario fez uma busca
+            if (showSearchMobileOverlay) {
+                evalQuery()
+                setShowSearchMobileOverlay(false)
+            }
+            // se não estava aparecendo, exibir o input mobile
+            else {
+                setShowSearchMobileOverlay(true)
+            }
+        }
+        // se a tela for grande, ao clicar no botão já executa a busca do input
+        else {
+            evalQuery('filtro')
+        }
     }
 
     return (
@@ -22,7 +40,7 @@ function Header() {
                     </h1>
                 </Link>
                 <SearchContainer
-                    onSearchMobileButtonClick={() => setShowSearchMobileOverlay(prev => !prev)}
+                    onSearch={searchQuery}
                 />
                 <Link to="/favoritos" className={`${styles.navLink} ${styles.listaFavoritosContainer}` }>
           
@@ -33,7 +51,7 @@ function Header() {
                 {showSearchMobileOverlay && (
                     <div className={styles.searchMobileOverlay}>
                         <input type="text" placeholder='Pesquisar na CineVortex' />
-                        <button onClick={searchUserQuery}><svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
+                        <button onClick={searchQuery}><svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
                     </div>
                 )}                   
             </nav>
--- src/components/SearchContainer/index.jsx
+++ src/components/SearchContainer/index.jsx
@@ -5,7 +5,7 @@ import FilterButton from "../FilterButton";
 import { useEffect, useState } from "react";
 
 
-function SearchContainer({ onSearchMobileButtonClick }) {
+function SearchContainer({ onSearch }) {
 
     const filterItems = [
         {
@@ -69,7 +69,7 @@ function SearchContainer({ onSearchMobileButtonClick }) {
             </div>
             <div className={styles.searchInputContainer}>
                 <input type="text" placeholder='Pesquisar na CineVortex' />
-                <button onClick={onSearchMobileButtonClick}><svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
+                <button onClick={onSearch}><svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
             </div>
         </div>
     )
