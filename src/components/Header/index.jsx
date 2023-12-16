c7e464ad7d4ccb90ab7da749d527a2c5eadf1d9d
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2023-12-16 11:17:36 -0300

    chore: incluindo controlled inputs nos inputs de pesquisa

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,10 @@ import { Link } from "react-router-dom";
 function Header() {
 
     const [showSearchMobileOverlay, setShowSearchMobileOverlay] = useState(false);
+    const [inputs, setInputs] = useState({
+        mobileInput: '',
+        desktopInput: ''
+    })
 
     function evalQuery (filter='') {
         console.log('filtro: >' + filter + '<')
@@ -30,6 +34,14 @@ function Header() {
         }
     }
 
+    function onInputChange(event) {
+        setInputs(prev => {
+            const previous = { ...prev }
+            previous[event.target.id] = event.target.value
+            return previous
+        })
+    }
+
     return (
         <header className={styles.header}>
             <nav className={styles.navigationContainer}>
@@ -41,6 +53,8 @@ function Header() {
                 </Link>
                 <SearchContainer
                     onSearch={searchQuery}
+                    inputValue={inputs.desktopInput}
+                    inputChange={onInputChange}
                 />
                 <Link to="/favoritos" className={`${styles.navLink} ${styles.listaFavoritosContainer}` }>
           
@@ -50,7 +64,7 @@ function Header() {
                 </Link>
                 {showSearchMobileOverlay && (
                     <div className={styles.searchMobileOverlay}>
-                        <input type="text" placeholder='Pesquisar na CineVortex' />
+                        <input onChange={onInputChange} value={inputs.mobileInput} id='mobileInput' type="text" placeholder='Pesquisar na CineVortex' />
                         <button onClick={searchQuery}><svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>
                     </div>
                 )}                   
